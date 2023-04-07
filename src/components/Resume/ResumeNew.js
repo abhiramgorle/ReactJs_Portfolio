6782d9f02176ea86a173bd060731fbbe07da8863
abhiramgorle <80030901+abhiramgorle@users.noreply.github.com>
2023-04-07 12:42:32 +0530

    yet anothter deploy changes

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "src/Assets/Resume/AbhiResume.pdf";
+import pdf from "./AbhiResume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "src/Assets/Resume/AbhiResume.pdf";
+  "./AbhiResume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
