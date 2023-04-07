e472fdb1e8d924038c7c9ab1724adcb822fcd2a1
abhiramgorle <80030901+abhiramgorle@users.noreply.github.com>
2023-04-07 11:58:11 +0530

    Some changes to resume and readme images

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "C:/Users/asust/Downloads/my-portfolio/src/Assets/Durga_Abhiram_SDE_Resume.pdf";
+import pdf from "C:/Users/asust/Downloads/ReactJs_Portfolio/src/Assets/Durga_Abhiram_SDE_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "C:/Users/asust/Downloads/my-portfolio/src/Assets/Durga_Abhiram_SDE_Resume.pdf";
+  "src/Assets/Durga_Abhiram_SDE_Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
