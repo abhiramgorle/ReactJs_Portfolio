027657af2742d1a1ebe67d301c653fe17b99e5a9
abhiramgorle <80030901+abhiramgorle@users.noreply.github.com>
2023-04-07 12:02:45 +0530

    path change to resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "C:/Users/asust/Downloads/ReactJs_Portfolio/src/Assets/Durga_Abhiram_SDE_Resume.pdf";
+import pdf from "../../Assets/Resume/Durga_Abhiram_SDE_Resume";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "src/Assets/Durga_Abhiram_SDE_Resume.pdf";
+  "../../Assets/Resume/Durga_Abhiram_SDE_Resume";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
